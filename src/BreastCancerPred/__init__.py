62e12b247b0f145736f6490be39097d15f9e854e
Akshay-a-j <akshay.jagajampi2@gmail.com>
2024-07-02 20:23:05 +0530

    added logger

---
+++ src/BreastCancerPred/__init__.py
@@ -0,0 +1,18 @@
+import os
+import logging
+from datetime import datetime
+
+
+Log_str = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", Log_str)
+os.makedirs(logs_path, exist_ok = True)
+
+log_file_path = os.path.join(logs_path, Log_str)
+
+logging.basicConfig(
+    filename = log_file_path, 
+    format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO )
+
+if __name__ =="__main__":
+    logging.info("The logging has started")
---
+++ src/BreastCancerPred/utils/common.py
@@ -0,0 +1,5 @@
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+
